aad98249a3c59e45d8f86f70c905e29c24830d69
abhinitha19 <abhinithababuks@gmail.com>
2025-07-26 23:38:42 +0530

    web app completed

---
+++ app.py
@@ -0,0 +1,78 @@
+from flask import Flask, render_template, jsonify, request
+from src.helper import download_hugging_face_embeddings
+from langchain_pinecone import PineconeVectorStore
+from langchain_groq import ChatGroq
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+from src.prompt import *
+import os
+
+app = Flask(__name__)
+
+load_dotenv()
+
+PINECONE_API_KEY = os.environ.get('PINECONE_API_KEY')
+GROQ_API_KEY = os.environ.get('GROQ_API_KEY')
+
+# Set them in environment
+os.environ["PINECONE_API_KEY"] = PINECONE_API_KEY
+os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+embeddings = download_hugging_face_embeddings()
+index_name = "medical-chatbot"
+
+# Load the vector store
+docsearch = PineconeVectorStore.from_existing_index(
+    index_name=index_name,
+    embedding=embeddings
+)
+# Set up retriever
+retriever = docsearch.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+
+# ✅ Use Groq Model
+chatModel = ChatGroq(
+    model_name="deepseek-r1-distill-llama-70b",
+    temperature=0.7
+)
+system_prompt = (
+    "You are an Medical assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
+
+
+# Prompt setup
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
+
+# Create the chain
+question_answer_chain = create_stuff_documents_chain(chatModel, prompt)
+rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+
+@app.route("/")
+def index():
+    return render_template('chat.html')
+
+@app.route("/get", methods=["GET", "POST"])
+def chat():
+    msg = request.form["msg"]
+    print("User Input:", msg)
+    response = rag_chain.invoke({"input": msg})
+    print("Response:", response["answer"])
+    return str(response["answer"])
+
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
+
