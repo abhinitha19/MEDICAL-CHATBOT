cfec80f5312a179ac6d48ba27637ffeccaa25d25
abhinitha19 <abhinithababuks@gmail.com>
2025-07-29 15:45:53 +0530

    cicd added

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="medical_chatbot",
     version="0.1.0",
-    author="",
-    author_email="",
+    author="abhinitha19",
+    author_email="abhinithababuks@gmail.com",
     packages=find_packages(),
     install_requires=[]
 )
